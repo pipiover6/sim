763a77a95cf2c432285ac217e5b1777d64f854fb
pipiover6 <119631857+pipiover6@users.noreply.github.com>
2023-09-08 19:17:22 +0300

    Update normalFromUniform.py

--- normalFromUniform.py
+++ normalFromUniform.py
@@ -4,10 +4,12 @@ import scipy as sc
 def normalFromUniform(size=None):
   theta = 2 * np.pi * np.random.uniform(size=size)
   r = np.sqrt(-2 * np.log(np.random.uniform(size=size)))
-
+  return np.multiply(r, np.cos(theta))
+	
 def test():
 	M=8000
 	a = np.sort(normalFromUniform(M))
+	print(f"mean={np.mean(a)}, std={np.std(a)}")
 	b = np.sort(np.random.normal(size=M))
 	percentiles = [1,5,10,25,30,40,50]
 	for i in percentiles:
